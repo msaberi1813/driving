59e28a7e27e6bdfb63092e20aa67ba474d04b025
msaberi1813 <msaberi1813@gmail.com>
2018-07-16 11:08:37 +0430

    First commit: UI

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,6 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
+
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
